d3986e4f4dd73d437e9813e4e15ccd53899406a4
cferg9 <cmfergie9@gmail.com>
2025-04-29 14:28:53 -0500

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+// This class represents a customer's order with their name and a list of items. It includes
+// methods that get the respective information for the processing system. 
+
 public class Order implements Comparable<Order> {
     private static int idCounter = 1;
     private int orderID;
@@ -7,13 +10,16 @@ public class Order implements Comparable<Order> {
     private List<String> items;
     private long orderTime;
 
+  
+// This block of code builds an order with the specified customer name and items they ordered    
     public Order(String customerName, List<String> items) {
         this.orderID = idCounter++;
         this.customerName = customerName;
         this.items = new ArrayList<>(items);
         this.orderTime = System.currentTimeMillis();
     }
-
+ // This block of code is creating methods for an OrderID, customer name, items, and order time
+ // to then be able to return it so it is displayed. 
     public int getOrderID() { return orderID; }
     public String getCustomerName() { return customerName; }
     public List<String> getItems() { return items; }
@@ -29,3 +35,4 @@ public class Order implements Comparable<Order> {
         return "Order ID: " + orderID + ", Customer: " + customerName + ", Items: " + items;
     }
 }
+
